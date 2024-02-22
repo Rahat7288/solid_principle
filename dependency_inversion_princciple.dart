63fdfc6e4be65ccdb6f741d91e7cf8801e6af38b
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-02-22 23:49:42 +0600

    dependency inversion principle

---
+++ dependency_inversion_princciple.dart
@@ -0,0 +1,10 @@
+/**
+ * High-level modules should not depend on low-level modules. Both should depend on abstractions. 
+ * Abstractions should not depend on details. Details should depend on abstractions.
+ * 
+ * Real life example
+ * ================================
+ * Imagine building a house. You wouldn't directly depend on specific materials like bricks or wood for every component. 
+ * Instead, you would rely on abstract concepts like "walls" and "doors," which can be implemented using various materials
+ * depending on the situation. This is similar to how DIP promotes relying on abstractions rather than concrete implementations.
+ * */ 
